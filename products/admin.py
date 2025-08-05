7e728a1d7250827460025e61736f25a753840e51
K SAI KIRAN <120441973+ksaikiran950@users.noreply.github.com>
2025-08-05 22:51:09 +0530

    17925

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import *
 
+from django.contrib import adminfrom .models import MenuItem
 
-# Custom Admins
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'restaurant', 'price', 'is_available', 'created_at']
+    list_filter = ['restaurant', 'is_available']
+    search_fields = ['name', 'description']
+
+
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
 
