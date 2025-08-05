3904fad98ec5c4f7dd36313ecef62aefbc794a92
K SAI KIRAN <120441973+ksaikiran950@users.noreply.github.com>
2025-08-05 21:56:32 +0530

    17923

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
+# views.py
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+        try:
+            response = requests.get('http://localhost:8000/menu-items/')  # Replace with your actual API endpoint
+            menu_items = response.json()
+        except:
+            menu_items = []
+
+        return render(request, 'home.html', {'menu_items': menu_items})
