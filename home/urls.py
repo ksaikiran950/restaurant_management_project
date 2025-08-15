8c0dac08d2d282d48435b03f3e665872447f8121
K SAI KIRAN <120441973+ksaikiran950@users.noreply.github.com>
2025-08-15 14:54:52 +0530

    32393

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('contact/',views.contact_us,name='contact_us')
+    path('', views.homepage, name='homepage'),
+    path('reservations/', views.reservations, name='reservations'),
     
 ]
