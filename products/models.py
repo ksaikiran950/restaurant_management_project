2d8149319dcdf366f5dd25e1506125f4f8e6b270
K SAI KIRAN <120441973+ksaikiran950@users.noreply.github.com>
2025-08-17 19:48:42 +0530

    33650

--- products/models.py
+++ products/models.py
@@ -18,3 +18,14 @@ class RestaurantLocation(models.Model):
 
     def __str__(self):
         return f"{self.address},{self.city},{self.state} - {self.zip_code}"
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # New field
+    def __str__(self):
+        return self.name
+
+
