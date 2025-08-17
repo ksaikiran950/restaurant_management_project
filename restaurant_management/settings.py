791cea8f4ee7ad1fa171bf808e0fd72432da5e45
K SAI KIRAN <120441973+ksaikiran950@users.noreply.github.com>
2025-08-17 19:59:12 +0530

    33650

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 'DIRS':[BASE_DIR / 'home'/ 'templates']
@@ -131,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Add this at the bottom
 RESTAURANT_NAME = "Tasty Treats Restaurant"
 RESTAURANT_PHONE = "+91 98765 43210"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
