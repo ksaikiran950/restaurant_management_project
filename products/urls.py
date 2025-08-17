051e391e8f87de3ab3b49ad73732997b237bd1f6
K SAI KIRAN <120441973+ksaikiran950@users.noreply.github.com>
2025-08-17 19:57:50 +0530

    33650

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/',views.menu_list,name='menu_list'),
     path('contactc',views_contact_view,name='contact'),
+    path('menu/',view.menu_view,name='menu'),
 
 ]
