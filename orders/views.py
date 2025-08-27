7ffd69a439cf4e778a552f2803fe9ee0c4f5ad9e
K SAI KIRAN <120441973+ksaikiran950@users.noreply.github.com>
2025-08-27 18:37:25 +0530

    39285

--- orders/views.py
+++ orders/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def place_order(request):
     return render(request,'orders/place_order.html')
+
+
+def order_confirmation(request):
+    order_number = random.randint(100000,999999)
+    return render(request,'order/order_confirmation.html',{'order_number':order_number})
